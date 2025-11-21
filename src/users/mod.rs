15a5e07db261377d3649d30988f120e3d078ee1d
jakubmanczak <jakub@manczak.net>
2025-11-21 17:19:22 +0100

    thiserror, userstructerror

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use rusqlite::Connection;
 use uuid::Uuid;
 
 pub mod pwd;
@@ -7,8 +10,28 @@ pub struct User {
     handle: String,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum UserStructError {
+    #[error("Failed to execute SQL: {0}")]
+    UserSqlError(#[from] rusqlite::Error),
+    #[error("Non-UUID User PK found in DB")]
+    NonUuidPrimaryKey,
+}
+
 impl User {
     pub fn is_infradmin(&self) -> bool {
         self.id.is_max()
+    }
+    pub fn get_by_uuid(uuid: &Uuid, conn: &Connection) -> Result<User, UserStructError> {
+        let pk = uuid.to_string();
+        let (id, handle) = conn
+            .prepare("SELECT * FROM users WHERE id = ?1")?
+            .query_row([&pk], |row| {
+                Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
+            })?;
+        Ok(User {
+            id: Uuid::from_str(&id).map_err(|_| UserStructError::NonUuidPrimaryKey)?,
+            handle,
+        })
     }
 }
