c7b2ba53485a92f71e65d7b0fe134441890e9e43
jakubmanczak <jakub@manczak.net>
2025-11-21 18:03:19 +0100

    get session by token

--- src/users/sessions.rs
+++ src/users/sessions.rs
@@ -56,4 +56,28 @@ impl Session {
             revoked_at: res.5.map(|ts| DateTime::from_timestamp(ts, 0).unwrap()),
         })
     }
+    pub fn get_by_token(token: &str, conn: &Connection) -> Result<Session, SessionStructError> {
+        const QUERY: &str = "
+            SELECT (id, user_id, expiry, last_access, revoked, revoked_at)
+            FROM sessions WHERE token = ?1
+        ";
+        let res = conn.prepare(QUERY)?.query_one([token], |row| {
+            Ok((
+                row.get::<_, String>(0)?,      // id
+                row.get::<_, String>(1)?,      // user_id
+                row.get::<_, i64>(2)?,         // expiry
+                row.get::<_, i64>(3)?,         // last_access
+                row.get::<_, bool>(4)?,        // revoked
+                row.get::<_, Option<i64>>(5)?, // revoked_at
+            ))
+        })?;
+        Ok(Session {
+            id: Uuid::from_str(&res.0).map_err(|_| SessionStructError::NonUuidPrimaryKey)?,
+            user_id: Uuid::from_str(&res.1).map_err(|_| SessionStructError::NonUuidUserId)?,
+            expiry: DateTime::from_timestamp(res.2, 0).unwrap(),
+            last_access: DateTime::from_timestamp(res.3, 0).unwrap(),
+            revoked: res.4,
+            revoked_at: res.5.map(|ts| DateTime::from_timestamp(ts, 0).unwrap()),
+        })
+    }
 }
